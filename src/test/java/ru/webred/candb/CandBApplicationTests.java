5228059a7591eb477edeec0b4510258826128a75
denispashin <dpashin@gmail.com>
2022-11-06 14:25:14 +0500

    Initial commit

---
+++ src/test/java/ru/webred/candb/CandBApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.webred.candb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
